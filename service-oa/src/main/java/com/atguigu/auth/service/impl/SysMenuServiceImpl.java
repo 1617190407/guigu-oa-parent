4f0c573edb3ecfb082499a26d076255fabedda6a
秋白 <1617190407@qq.com>
2024-03-06 17:02:57 +0800

    add:菜单管理模块

---
+++ service-oa/src/main/java/com/atguigu/auth/service/impl/SysMenuServiceImpl.java
@@ -0,0 +1,98 @@
+package com.atguigu.auth.service.impl;
+
+
+import com.atguigu.auth.mapper.SysMenuMapper;
+import com.atguigu.auth.service.SysMenuService;
+import com.atguigu.auth.service.SysRoleMenuService;
+import com.atguigu.auth.utils.MenuHelper;
+import com.atguigu.common.config.exception.GuiguException;
+import com.atguigu.model.system.SysMenu;
+import com.atguigu.model.system.SysRoleMenu;
+import com.atguigu.vo.system.AssignMenuVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * <p>
+ * 菜单表 服务实现类
+ * </p>
+ *
+ * @author atguigu
+ * @since 2024-03-06
+ */
+@Service
+public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> implements SysMenuService {
+    @Autowired
+    private SysRoleMenuService sysRoleMenuService;
+    @Override
+    public List<SysMenu> findNodes() {
+        List<SysMenu> sysMenuList = baseMapper.selectList(null);
+        List<SysMenu> resultList = MenuHelper.buildTree(sysMenuList);
+        return resultList;
+    }
+
+    @Override
+    public void removeMenuById( Long id ) {
+        LambdaQueryWrapper<SysMenu> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(SysMenu::getParentId,id);
+        Integer count = baseMapper.selectCount(wrapper);
+        if(count > 0){
+            throw new GuiguException(201,"菜单不能删除");
+        }
+        baseMapper.deleteById(id);
+    }
+
+    @Override
+    public List<SysMenu> findMenuByRoleId( Long roleId ) {
+        //1 查询所有菜单- 添加条件  status =1
+        LambdaQueryWrapper<SysMenu> wrapperSysMenu = new LambdaQueryWrapper<>();
+        wrapperSysMenu.eq(SysMenu::getStatus,1);
+        List<SysMenu> allSysMenuList = baseMapper.selectList(wrapperSysMenu);
+
+        //2 根据角色id roleId查询 角色菜单关系表里面 角色id对应所有的菜单id
+        LambdaQueryWrapper<SysRoleMenu> wrapperSysRoleMenu = new LambdaQueryWrapper<>();
+        wrapperSysRoleMenu.eq(SysRoleMenu::getRoleId,roleId);
+        List<SysRoleMenu> sysRoleMenuList
+                = sysRoleMenuService.list(wrapperSysRoleMenu);
+
+        //3 根据获取菜单id,获取对应菜单对象
+        List<Long> menuIdList = sysRoleMenuList.stream().map(c -> c.getMenuId()).collect(Collectors.toList());
+
+        //3.1 拿着菜单id 和所以菜单集合里面id进行比较，如果相同封装
+        allSysMenuList.stream().forEach(item -> {
+            if(menuIdList.contains(item.getId())){
+                item.setSelect(true);
+            }else{
+                item.setSelect(false);
+            }
+        });
+        //返回规定格式菜单列表
+        List<SysMenu> sysMenuList = MenuHelper.buildTree(allSysMenuList);
+        return sysMenuList;
+    }
+
+    @Override
+    public void doAssign( AssignMenuVo assignMenuVo ) {
+        //1.根据角色 id删除菜单
+        LambdaQueryWrapper<SysRoleMenu> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(SysRoleMenu::getRoleId,assignMenuVo.getRoleId());
+        sysRoleMenuService.remove(wrapper);
+        //2 从参数里面获取角色新分配菜单id列表,进行遍历,把每个id 数据添加菜单角色表
+        List<Long> menuIdList = assignMenuVo.getMenuIdList();
+        for(Long menuId:menuIdList){
+            if(StringUtils.isEmpty(menuId)){
+                continue;
+            }
+            SysRoleMenu sysRoleMenu = new SysRoleMenu();
+            sysRoleMenu.setMenuId(menuId);
+            sysRoleMenu.setRoleId(assignMenuVo.getRoleId());
+            sysRoleMenuService.save(sysRoleMenu);
+        }
+    }
+}
